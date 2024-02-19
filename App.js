88f4874a48db47532be8d18cabd17694ddbf1e38
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-19 10:58:11 -0300

    Update App.js
    Para visualização  da tela Sobre

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
 import Home from "./src/screens/Home";
+import Sobre from "./src/screens/Sobre";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content"></StatusBar>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <Sobre></Sobre>
     </>
   );
 }
