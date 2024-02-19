d5ec967638db9b9acc653cf1706e4bfa83e156d5
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-19 11:45:17 -0300

    Permitindo scroll da tela

--- src/screens/Sobre.js
+++ src/screens/Sobre.js
@@ -6,6 +6,7 @@ import {
   View,
   Linking,
   Pressable,
+  ScrollView,
 } from "react-native";
 import SafeContainer from "../components/SafeContainer";
 import tmdb from "../../assets/images/logo-tmdb.png";
@@ -14,33 +15,35 @@ export default function Sobre() {
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
-        <Text style={estilos.subTitulo}>Sobre o app DaHora Filmes</Text>
-        <Text style={estilos.texto}>
-          O <Text style={estilos.nomeApp}>DaHora Filmes</Text> é um aplicativo
-          com a finalidade de permitir a busca por informações sobre filmes
-          existentes na base de dados pública disponibilizada pelo site The
-          Movie Database (TMDb).
-        </Text>
+        <ScrollView>
+          <Text style={estilos.subTitulo}>Sobre o app DaHora Filmes</Text>
+          <Text style={estilos.texto}>
+            O <Text style={estilos.nomeApp}>DaHora Filmes</Text> é um aplicativo
+            com a finalidade de permitir a busca por informações sobre filmes
+            existentes na base de dados pública disponibilizada pelo site The
+            Movie Database (TMDb).
+          </Text>
 
-        <Pressable
-          onPress={() => Linking.openURL("https://www.themoviedb.org/")}
-        >
-          <View style={estilos.logoTmdb}>
-            <Image source={tmdb}></Image>
-          </View>
-        </Pressable>
+          <Pressable
+            onPress={() => Linking.openURL("https://www.themoviedb.org/")}
+          >
+            <View style={estilos.logoTmdb}>
+              <Image resizeMode="contain" source={tmdb}></Image>
+            </View>
+          </Pressable>
 
-        <Text style={estilos.texto}>
-          Ao localizar um filme, o usuário pode visualizar informações como
-          título, data de lançamento, nota média de avaliação e uma breve
-          descrição sobre o filme e, caso queira, salvar estas informações em
-          uma lista no próprio aplicativo para visualização posterior.
-        </Text>
-        <Text style={estilos.texto}>
-          O aplicativo poderá receber novos recursos conforme o feedback e
-          demanda dos usuários.
-        </Text>
-        <Text style={estilos.texto}>DaHora Filmes &copy; 2024</Text>
+          <Text style={estilos.texto}>
+            Ao localizar um filme, o usuário pode visualizar informações como
+            título, data de lançamento, nota média de avaliação e uma breve
+            descrição sobre o filme e, caso queira, salvar estas informações em
+            uma lista no próprio aplicativo para visualização posterior.
+          </Text>
+          <Text style={estilos.texto}>
+            O aplicativo poderá receber novos recursos conforme o feedback e
+            demanda dos usuários.
+          </Text>
+          <Text style={estilos.texto}>DaHora Filmes &copy; 2024</Text>
+        </ScrollView>
       </View>
     </SafeContainer>
   );
