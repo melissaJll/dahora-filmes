a2f320bed2bd883a54e3725e4f2b617d9b73005d
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-19 11:55:59 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
+import Privacidade from "./src/screens/Privacidade";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content"></StatusBar>
       {/* <Home></Home> */}
-      <Sobre></Sobre>
+      <Privacidade></Privacidade>
+      {/* <Sobre></Sobre> */}
     </>
   );
 }
