03e96d875873237f79189c1a0cf4f948c2654072
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-02-26 09:18:18 -0300

    configuração navigator.stack

--- App.js
+++ App.js
@@ -1,4 +1,10 @@
 import { StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Criação/inicialização do mecanismo stack
+const Stack = createNativeStackNavigator();
+
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
 import Privacidade from "./src/screens/Privacidade";
@@ -7,9 +13,12 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      {/* <Home /> */}
-      {/* <Sobre /> */}
-      <Privacidade />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* Cada página que será usada precisa de um stack.screen */}
+          <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
