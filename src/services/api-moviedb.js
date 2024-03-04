03411a928acf78321c500222c58c450453a68744
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-04 11:20:03 -0300

    Update api-moviedb.js

--- src/services/api-moviedb.js
+++ src/services/api-moviedb.js
@@ -3,3 +3,6 @@ import axios from "axios";
 //configuramos também o uso da API Key
 
 const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+});
