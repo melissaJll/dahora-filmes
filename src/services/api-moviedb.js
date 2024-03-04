a5d9509a5107fb46b2e193703ef579b7fa1e4574
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-04 11:19:26 -0300

    Update api-moviedb.js URL

--- src/services/api-moviedb.js
+++ src/services/api-moviedb.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 //usamos o axios para criar uma referencia ao endpoint da API/SERVICE (chamada de baseURL) e
 //configuramos também o uso da API Key
+
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
