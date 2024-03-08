847d795c2626091a51375e8105441f3154352e3c
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-03-08 10:29:06 -0300

    estilização do card

--- src/components/CardFilme.js
+++ src/components/CardFilme.js
@@ -8,9 +8,12 @@ export default function CardFilme() {
       <Image style={estilos.image} source={imagemAlternativa} />
       <View style={estilos.corpo}>
         <Text style={estilos.titulo}>Nome do filme</Text>
-        <View>
+        <View style={estilos.botoes}>
           <Pressable style={estilos.botao}>
             <Text style={estilos.textoBotao}> Leia mais</Text>
+          </Pressable>
+          <Pressable style={estilos.botao}>
+            <Text style={estilos.textoBotao}> Salvar</Text>
           </Pressable>
         </View>
       </View>
@@ -18,4 +21,46 @@ export default function CardFilme() {
   );
 }
 
-const estilos = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  card: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    borderWidth: 2,
+    borderColor: "#5451a6",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  image: {
+    height: 150,
+    width: 100,
+    flex: 1,
+  },
+  corpo: {
+    flex: 2,
+  },
+
+  botao: {
+    borderColor: "#5451a6",
+    borderWidth: 1,
+    padding: 8,
+  },
+  textoBotao: {
+    color: "#5451a6",
+    fontSize: 12,
+    textTransform: "uppercase",
+  },
+  titulo: {
+    marginLeft: 3,
+    backgroundColor: "#5451a6",
+    color: "white",
+    textAlign: "center",
+    paddingVertical: 8,
+    fontSize: 16,
+  },
+  botoes: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginTop: 8,
+  },
+});
